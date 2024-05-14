d47dd50c21ef993fb63bd34184b2b13f418f239f
ferdyamc <ferdyamc.dev@gmail.com>
2024-05-13 23:15:44 -0500

    Corrección de comentarios

--- js/copy-link.js
+++ js/copy-link.js
@@ -1,7 +1,7 @@
 let btnsheet = document.getElementById("btn-link-sheet");//botón para copiar la hója de calculo
 let btndrive = document.getElementById("btn-link-drive");//botón para copiar la carpeta de drive
 
-//Evento para copiar link de la hoja de cáculo y cambiar el estilo de ícono "copiar" por 1 segundo al hacer click en el ícono
+//Evento para copiar link de la hoja de cáculo y cambiar el estilo del ícono "copiar" por 1 segundo al hacer click en el ícono
 btnsheet.addEventListener("click" , ()=>{
     navigator.clipboard.writeText(document.getElementById("text-link-sheet").textContent);
     btnsheet.classList.remove("fa-regular");
@@ -15,7 +15,7 @@ btnsheet.addEventListener("click" , ()=>{
     },1000)
 })
 
-//Evento para copiar link de la carpeta de drive y cambiar el estilo de ícono "copiar" por 1 segundo al hacer click en el ícono
+//Evento para copiar link de la carpeta de drive y cambiar el estilo del ícono "copiar" por 1 segundo al hacer click en el ícono
 btndrive.addEventListener("click" , ()=>{
     navigator.clipboard.writeText(document.getElementById("text-link-drive").textContent);
     btndrive.classList.remove("fa-regular");
